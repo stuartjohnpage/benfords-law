15c1f931ff9101a5630339881cb2b432e5ec31d1
Stuart Page <stuart.page@revelry.co>
2021-11-17 11:14:38 -0600

    uncommented out second digit stuff

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import matplotlib.pyplot as plt
 BENFORD_FIRST = [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6]
 BENFORD_SECOND = [11.9, 11.3, 10.8, 10.4, 10.0, 9.67, 9.3, 9.0, 8.7, 8.5]
 
+
 def load_data(filename):
     """Open a text file & return a list of strings"""
     with open(filename) as f:
         return f.read().strip().split('\n')
 
-def count_first_digits(data_list):
-    first_digits = defaultdict(int)
+
+def count_digits(data_list, method):
+    digits = defaultdict(int)
     for sample in data_list:
         if sample == '':
             continue
@@ -22,27 +24,15 @@ def count_first_digits(data_list):
             print(e, file=sys.stderr)
             print("Sample must be integers. Exiting...", file=sys.stderr)
             sys.exit(1)
-        first_digits[sample[0]] += 1
-    first_digits.pop("0", None)
-    data_count = [v for (k, v) in sorted(first_digits.items())]
-
-    total_count = sum(data_count)
-    data_pct = [(i / total_count) * 100 for i in data_count]
-    return data_count, data_pct, total_count
-
-def count_second_digits(data_list):
-    second_digits = defaultdict(int)
-    for sample in data_list:
-        if sample == '':
+        if method == "first":
+            digits[sample[0]] += 1
+        elif method == "second" and len(sample) > 1:
+            digits[sample[1]] += 1
+        else:
             continue
-        try:
-            int(sample)
-        except ValueError as e:
-            print(e, file=sys.stderr)
-            print("Sample must be integers. Exiting...", file=sys.stderr)
-            sys.exit(1)
-        second_digits[sample[1]] += 1
-    data_count = [v for (k, v) in sorted(second_digits.items())]
+    if method == "first":
+        digits.pop("0", None)
+    data_count = [v for (k, v) in sorted(digits.items())]
 
     total_count = sum(data_count)
     data_pct = [(i / total_count) * 100 for i in data_count]
@@ -54,6 +44,7 @@ def get_expected_counts(total_count, method):
         return [round(p * total_count / 100) for p in BENFORD_FIRST]
     else:
         return [round(p * total_count / 100) for p in BENFORD_SECOND]
+
 
 def chi_square_test(data_count, expected_counts, method):
     if method == "first":
@@ -75,6 +66,7 @@ def chi_square_test(data_count, expected_counts, method):
 
         return chi_square_stat < 16.92
 
+
 def bar_chart(data_pct, method):
     fig, ax = plt.subplots()
     index = [i + 1 for i in range(len(data_pct))]
@@ -92,16 +84,17 @@ def bar_chart(data_pct, method):
         ax.text(rect.get_x() + rect.get_width()/2, height,
                 '{:0.1f}'.format(height), ha='center', va='bottom', fontsize=13)
 
-    if method =="first":
+    if method == "first":
         ax.scatter(index, BENFORD_FIRST, s=150, c='red', zorder=2, label='Benford {}'.format(method))
     else:
         ax.scatter(index, BENFORD_SECOND, s=150, c='red', zorder=2, label='Benford {}'.format(method))
 
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
-    ax.legend(prop={'size':15}, frameon=False)
+    ax.legend(prop={'size': 15}, frameon=False)
 
     plt.show()
+
 
 def main():
     while True:
@@ -112,8 +105,7 @@ def main():
             print("{}. Try again.".format(e), file=sys.stderr)
         else:
             break
-    data_count, data_pct, total_count = count_first_digits(data_list)
-    second_data_count, second_data_pct, second_total_count = count_second_digits(data_list)
+    data_count, data_pct, total_count = count_digits(data_list, "first")
 
     first_expected_counts = get_expected_counts(total_count, "first")
 
@@ -125,25 +117,26 @@ def main():
         print("{}: observed: {:.3f} expected: {:.3f}".
               format(i, data_pct[i - 1] / 100, BENFORD_FIRST[i - 1] / 100))
     if chi_square_test(data_count, first_expected_counts, "first"):
-        print("Observed distribution matches expected distribution")
+        print("Observed distribution matches expected distribution for first digit.")
     else:
-        print("Observed distribution does not match expected.", file=sys.stderr)
+        print("Observed distribution does not match expected for first digit.", file=sys.stderr)
 
-    # second_expected_counts = get_expected_counts(second_total_count, "second")
-    # print("\nObserved second counts = {}".format(second_data_count))
-    # print("Expected counts = {}".format(second_expected_counts), "\n")
-    #
-    # print("Second Digit Probabilities")
-    # for i in range(0, 10):
-    #     print("{}: observed: {:.3f} expected: {:.3f}".
-    #           format(i, second_data_pct[i - 1] / 100, BENFORD_SECOND[i - 1] / 100))
-    # if chi_square_test(second_data_count, second_expected_counts, "second"):
-    #     print("Observed distribution matches expected distribution")
-    # else:
-    #     print("Observed distribution does not match expected.", file=sys.stderr)
-    #
-    # bar_chart(second_data_pct, "second")
+    second_data_count, second_data_pct, second_total_count = count_digits(data_list, "second")
+    second_expected_counts = get_expected_counts(second_total_count, "second")
+    print("\nObserved second counts = {}".format(second_data_count))
+    print("Expected counts = {}".format(second_expected_counts), "\n")
+
+    print("Second Digit Probabilities")
+    for i in range(0, 10):
+        print("{}: observed: {:.3f} expected: {:.3f}".
+              format(i, second_data_pct[i - 1] / 100, BENFORD_SECOND[i - 1] / 100))
+    if chi_square_test(second_data_count, second_expected_counts, "second"):
+        print("Observed distribution matches expected distribution for second digit.")
+    else:
+        print("Observed distribution does not match expected for second digit.", file=sys.stderr)
+
     bar_chart(data_pct, "first")
+    bar_chart(second_data_pct, "second")
 
     sys.exit(0)
 
@@ -151,22 +144,23 @@ def main():
 if __name__ == '__main__':
     main()
 
+# I had to wrangle some data due to the specific way the Louisiana SOS formats their files.
 
-def data_wrangler():
-    while True:
-        filename = input("\nPlease enter the name of the file you would like to wrangle ")
-        try:
-            data_list = load_data(filename)
-        except IOError as e:
-            print("{}. Try again.".format(e), file=sys.stderr)
-        else:
-            break
-    wrangled_data = []
-
-    for i in range(0, len(data_list), 3 ):
-        wrangled_data.append(data_list[i])
-    print(wrangled_data)
-
-    with open('orleans_2008_votes.txt', 'w') as f:
-        for item in wrangled_data:
-            f.write("%s\n" % item)
+# def data_wrangler():
+#     while True:
+#         filename = input("\nPlease enter the name of the file you would like to wrangle ")
+#         try:
+#             data_list = load_data(filename)
+#         except IOError as e:
+#             print("{}. Try again.".format(e), file=sys.stderr)
+#         else:
+#             break
+#     wrangled_data = []
+#
+#     for i in range(0, len(data_list), 3 ):
+#         wrangled_data.append(data_list[i])
+#     print(wrangled_data)
+#
+#     with open('orleans_2008_votes.txt', 'w') as f:
+#         for item in wrangled_data:
+#             f.write("%s\n" % item)
